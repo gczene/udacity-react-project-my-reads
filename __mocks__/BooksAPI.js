23709770e8d9ada5186f734cf1c9a8fdaace91cc
gczene <gabor@czene.co.uk>
2017-07-24 14:43:56 +0100

    test: base test

--- __mocks__/BooksAPI.js
+++
@@ -1,4 +0,0 @@
-
-export const getAll = () => {
-  console.log('CALLED');
-}
--- src/App.test.js
+++ src/App.test.js
@@ -4,9 +4,10 @@ import App from './App'
 import {mount} from 'enzyme';
 import {MemoryRouter} from 'react-router-dom';
 
+jest.mock('./BooksAPI');
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
   const app = mount(<MemoryRouter><App /></MemoryRouter>);
   expect(app.contains(<h1>MyReads</h1>)).toEqual(true);
-
 });
