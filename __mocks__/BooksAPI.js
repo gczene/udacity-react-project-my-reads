57122b810f24352e2dd88cec9bc2deb51d7ab1b3
gczene <gabor@czene.co.uk>
2017-07-24 14:13:05 +0100

    test setup

---
+++ __mocks__/BooksAPI.js
@@ -0,0 +1,4 @@
+
+export const getAll = () => {
+  console.log('CALLED');
+}
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-
-/** 
- This course is not designed to teach Test Driven Development. 
- Feel free to use this file to test your application, but it 
- is not required.
-**/
+import {mount} from 'enzyme';
+import {MemoryRouter} from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
+  const app = mount(<MemoryRouter><App /></MemoryRouter>);
+  expect(app.contains(<h1>MyReads</h1>)).toEqual(true);
 
-
+});
